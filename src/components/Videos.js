35444c8aa8a09f014dab7a5f402b3b2daa154a36
Manasa Vesala <vesalamanasa@gmail.com>
2019-03-08 09:02:49 -0800

    Added video component

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -5,9 +5,16 @@ UIkit.cover();
 
 function Videos() {
     return (
-        <div class="uk-cover-container uk-height-medium ">
+        <div>
             <br />
-            <iframe src="https://www.youtube-nocookie.com/embed/YE7VzlLtp-4?autoplay=1&amp;controls=0&amp;showinfo=0&amp;rel=0&amp;loop=1&amp;modestbranding=1&amp;wmode=transparent" width="560" height="315" frameborder="0" allowfullscreen uk-cover></iframe>
+            <h1>List of JavaScript videos</h1>
+            <h1>List of Java videos</h1>
+            <h1>List of c# videos</h1>
+            <h1>List of React videos</h1>
+            <div class="uk-cover-container uk-height-medium ">
+                <br />
+                <iframe src="https://www.youtube-nocookie.com/embed/YE7VzlLtp-4?autoplay=1&amp;controls=0&amp;showinfo=0&amp;rel=0&amp;loop=1&amp;modestbranding=1&amp;wmode=transparent" width="560" height="315" frameborder="0" allowfullscreen uk-cover></iframe>
+            </div>
         </div>
     );
 }
